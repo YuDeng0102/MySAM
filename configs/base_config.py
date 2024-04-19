2f74ea7f18d27f5ace9739f70fa6fb1a64ed1ae9
root <root@DESKTOP-IKTSDBB.localdomain>
2024-04-19 21:25:08 +0800

    first commit

---
+++ configs/base_config.py
@@ -0,0 +1,45 @@
+base_config = {
+    "eval_interval": 1,
+    "ema_rate": 0.999,
+    "csv_keys": ["Name", "Prompt", "Mean IoU", "Mean F1", "epoch"],
+    "opt": {
+        "learning_rate": 1e-4,
+        "weight_decay": 1e-4,
+        "decay_factor": 10,
+        "steps": [60000, 86666],
+        "warmup_steps": 250,
+    },
+    "corruptions": [
+        "gaussian_noise",
+        "shot_noise",
+        "impulse_noise",
+        "defocus_blur",
+        "glass_blur",
+        "motion_blur",
+        "zoom_blur",
+        "snow",
+        "frost",
+        "fog",
+        "brightness",
+        "contrast",
+        "elastic_transform",
+        "pixelate",
+        "jpeg_compression",
+    ],
+    "model": {
+        "type": "vit_b",
+        "checkpoint": "./checkpoints/",
+        "ckpt": "",
+        "freeze": {
+            "image_encoder": True,
+            "prompt_encoder": True,
+            "mask_decoder": True,
+        },
+    },
+    "datasets": {
+        "coco": {
+            "root_dir": "./data/coco2017/val2017",            
+            "annotation_file": "./data/coco2017/annotations/instances_val2017.json",
+        }
+    },
+}
---
+++ datasets/COCO.py
@@ -0,0 +1,124 @@
+import os
+import cv2
+import random
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from pycocotools.coco import COCO
+from skimage.draw import polygon2mask
+from datasets.tools import ResizeAndPad, soft_transform, collate_fn, collate_fn_soft, collate_fn_
+
+
+class COCODataset(Dataset):
+    def __init__(self, cfg, root_dir, dataset_type='train', transform=None,if_self_training=False):
+        
+        #检测文件是否存在
+        assert dataset_type in ["train", "val"], 'dataset must be in ["train", "val"]'
+        anno_file = f"instance_{dataset_type}.json"
+        assert os.path.exists(root_dir), "file '{}' does not exist.".format(root_dir)
+        self.img_root = os.path.join(root_dir, f"{dataset_type}")
+        assert os.path.exists(self.img_root), "path '{}' does not exist.".format(self.img_root)
+        self.anno_path = os.path.join(root_dir, "annotations", anno_file)
+        assert os.path.exists(self.anno_path), "file '{}' does not exist.".format(self.anno_path)
+       
+        
+        self.cfg = cfg
+        self.transform = transform
+        self.coco = COCO(self.anno_path)
+        image_ids = sorted(list(self.coco.imgs.keys()))
+
+        # Filter out image_ids without any annotations
+        self.image_ids = [
+            image_id
+            for image_id in image_ids
+            if len(self.coco.getAnnIds(imgIds=image_id)) > 0
+        ]
+
+        self.if_self_training = if_self_training
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        image_info = self.coco.loadImgs(image_id)[0]
+        image_path = os.path.join(self.root_dir, image_info["file_name"])
+      
+        image = cv2.imread(image_path)
+        # corrupt_image(image, image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        ann_ids = self.coco.getAnnIds(imgIds=image_id)
+        anns = self.coco.loadAnns(ann_ids)
+        bboxes = []
+        masks = []
+        categories = []
+        for ann in anns:
+            x, y, w, h = ann["bbox"]
+            bboxes.append([x, y, x + w, y + h])
+            mask = self.coco.annToMask(ann)
+            masks.append(mask)
+            categories.append(ann["category_id"])
+
+        if self.if_self_training:
+            image_weak, bboxes_weak, masks_weak, image_strong = soft_transform(image, bboxes, masks, categories)
+            # image_origin = image_weak
+
+            if self.transform:
+                image_weak, masks_weak, bboxes_weak = self.transform(image_weak, masks_weak, np.array(bboxes_weak))
+                image_strong = self.transform.transform_image(image_strong)
+
+            bboxes_weak = np.stack(bboxes_weak, axis=0)
+            masks_weak = np.stack(masks_weak, axis=0)
+            return image_weak, image_strong, torch.tensor(bboxes_weak), torch.tensor(masks_weak).float()
+
+        elif self.cfg.visual:
+            origin_image = image
+            origin_bboxes = bboxes
+            origin_masks = masks
+            if self.transform:
+                padding, image, masks, bboxes = self.transform(image, masks, np.array(bboxes), True)
+
+            bboxes = np.stack(bboxes, axis=0)
+            masks = np.stack(masks, axis=0)
+            origin_bboxes = np.stack(origin_bboxes, axis=0)
+            origin_masks = np.stack(origin_masks, axis=0)
+            return image_id, padding, origin_image, origin_bboxes, origin_masks, image, torch.tensor(bboxes), torch.tensor(masks).float()
+
+        else:
+            if self.transform:
+                image, masks, bboxes = self.transform(image, masks, np.array(bboxes))
+
+            bboxes = np.stack(bboxes, axis=0)
+            masks = np.stack(masks, axis=0)
+            return image, torch.tensor(bboxes), torch.tensor(masks).float()
+
+def load_datasets(cfg, img_size):
+    transform = ResizeAndPad(img_size)
+    train = COCODataset(
+        cfg,
+        root_dir=cfg.datasets.coco.root_dir,
+        dataset_type='train',
+        transform=transform
+    )
+    val = COCODataset(
+        cfg,
+        root_dir=cfg.datasets.coco.root_dir,
+         dataset_type='val',
+        transform=transform,
+    )
+    train_dataloader = DataLoader(
+        train,
+        batch_size=cfg.batch_size,
+        shuffle=True,
+        num_workers=cfg.num_workers,
+        collate_fn=collate_fn,
+    )
+    val_dataloader = DataLoader(
+        val,
+        batch_size=cfg.val_batchsize,
+        shuffle=True,
+        num_workers=cfg.num_workers,
+        collate_fn=collate_fn,
+    )
+    return train_dataloader, val_dataloader
